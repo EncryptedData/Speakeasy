f00a6019140c878e5131bbdd1cbecb948eb582ba
tyler.nagamine <tylernagamine@gmail.com>
2026-03-28 17:15:40 -0700

    v1 auth context

---
+++ Speakeasy.WebUI/src/context/authContext.tsx
@@ -0,0 +1,67 @@
+import {
+  createContext,
+  createMemo,
+  createResource,
+  ParentComponent,
+  useContext,
+} from "solid-js";
+import { createStore } from "solid-js/store";
+import { makePersisted } from "@solid-primitives/storage";
+import { getManageInfo } from "@api";
+
+export type AuthContext = {
+  accessToken: () => string;
+  authLoading: () => boolean;
+  email: () => string | undefined;
+  isLoggedIn: () => boolean;
+  refreshToken: () => string;
+  updateAuth: (newAuth: { accessToken: string; refreshToken: string }) => void;
+};
+
+const AuthContext = createContext<AuthContext>({
+  accessToken: () => "",
+  authLoading: () => true,
+  email: () => "",
+  isLoggedIn: () => false,
+  refreshToken: () => "",
+  updateAuth: () => {},
+});
+
+export const AuthProvider: ParentComponent = (props) => {
+  const [authStore, setAuthStore] = makePersisted(
+    createStore({
+      accessToken: "",
+      refreshToken: "",
+    }),
+  );
+
+  const manageInfoInput = createMemo(() =>
+    !!authStore.accessToken
+      ? ({
+          auth: () => authStore.accessToken,
+        } satisfies Parameters<typeof getManageInfo>[0])
+      : null,
+  );
+  const [{ latest, state }] = createResource(manageInfoInput, getManageInfo);
+
+  const value: AuthContext = {
+    accessToken: () => authStore.accessToken,
+    email: () => latest?.data?.email,
+    isLoggedIn: () => !!latest?.data?.email,
+    authLoading: () => state !== "unresolved" && state !== "ready",
+    refreshToken: () => authStore.refreshToken,
+    updateAuth: ({ accessToken, refreshToken }) =>
+      setAuthStore({
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      }),
+  };
+
+  return (
+    <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
+  );
+};
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
