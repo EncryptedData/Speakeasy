68256c28e2f597cf2aaa64cd709e32cf643a59a4
Russell J <russell.johnson@superdark.net>
2026-03-31 21:29:33 -0700

    Fixed build and added Db migration

--- Speakeasy.Server.Models/Database/Migrations/20260401042757_Added User profile image.Designer.cs
+++ Speakeasy.Server.Models/Database/Migrations/20260401042757_Added User profile image.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Speakeasy.Server.Models.Database;
@@ -11,9 +12,11 @@ using Speakeasy.Server.Models.Database;
 namespace Speakeasy.Server.Models.Database.Migrations
 {
     [DbContext(typeof(SpeakeasyDbContext))]
-    partial class SpeakeasyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260401042757_Added User profile image")]
+    partial class AddedUserprofileimage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
