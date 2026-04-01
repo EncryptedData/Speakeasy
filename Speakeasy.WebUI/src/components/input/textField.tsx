3aaabd4f6e93881a8a2a148e67bfa20528a6528c
tyler.nagamine <tylernagamine@gmail.com>
2026-03-31 21:05:57 -0700

    rename textField -> textInput. Create textField

--- Speakeasy.WebUI/src/components/input/textField.tsx
+++
@@ -1,7 +0,0 @@
-import { Component, JSX } from "solid-js";
-
-export type TextFieldProps = JSX.InputHTMLAttributes<HTMLInputElement>;
-
-export const TextField: Component<TextFieldProps> = (props) => {
-  return <input {...props} class={`p-4 bg-bg-input ${props.class || ""}`} />;
-};
